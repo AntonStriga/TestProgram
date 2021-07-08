5d125479f1adbb9afef2b06916d6073edd8079b1
DESKTOP-L3237SB\Glau <anton.striga@jaydevs.com>
2021-07-08 13:25:52 +0300

    tests wuth DataProvider

---
+++ src/test/java/tests/DataDrivenTests.java
@@ -0,0 +1,48 @@
+package tests;
+
+import io.restassured.http.ContentType;
+import org.json.simple.JSONObject;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.baseURI;
+import static io.restassured.RestAssured.given;
+
+public class DataDrivenTests extends DataProviders {
+
+    private final String BASE_URL = "http://localhost:3000/";
+
+    @Test(groups = {"POST"}, dataProvider = "DataForPost")
+    public void test_01(String firstName, String lastName, int subjectId) {
+        baseURI = BASE_URL;
+
+        JSONObject requestBody = new JSONObject();
+
+        requestBody.put("firstName", firstName);
+        requestBody.put("lastName", lastName);
+        requestBody.put("subjectId", subjectId);
+
+        given()
+            .header("Content-Type","application/json")
+            .contentType(ContentType.JSON)
+            .accept(ContentType.JSON)
+            .body(requestBody.toJSONString())
+        .when()
+            .post("/users")
+        .then()
+            .statusCode(201)
+        .log().all();
+    }
+
+    @Test (groups = {"DELETE"}, dataProvider = "DataForDelete")
+    public void test_02(int userToDeleteId) {
+        baseURI = BASE_URL;
+
+        given()
+                .when()
+                .delete("/users/" + userToDeleteId)
+                .then()
+                .statusCode(200)
+                .log().all();
+    }
+}
