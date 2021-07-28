7b2d2e5103da184c97f97bdad85d8dd3e89ce4f2
DESKTOP-L3237SB\Glau <anton.striga@jaydevs.com>
2021-07-28 12:40:32 +0300

    added dependencies and example

---
+++ src/test/java/Example.java
@@ -0,0 +1,26 @@
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Example {
+
+    @Test
+    public void firstTest() throws IOException {
+        CloseableHttpClient httpClient = HttpClients.createDefault();
+        HttpGet httpGet = new HttpGet(("https://www.google.com/"));
+        HttpResponse httpResponse = httpClient.execute(httpGet);
+
+        Scanner sc = new Scanner((httpResponse.getEntity().getContent()));
+
+        System.out.println(httpResponse.getStatusLine());
+        while(sc.hasNext()) {
+            System.out.println(sc.nextLine());
+        }
+    }
+}
